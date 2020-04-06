d189794d511fb4fda352e2f6a17bd9f66a79672e
Katy Rumble <rumble.katy@gmail.com>
2020-04-06 01:35:05 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{ name: 'Laptop' }, { name: 'Desktop' }, { name: 'Monitor' }])
+
+manufacturers = Manufacturer.create([{ name: 'Apple' }, { name: 'Acer' }, { name: 'Samsung' }])
+
+models = Model.create([
+  { manufacturer: manufacturers.first, name: 'MacBook Pro (16-inch, 2019)' },
+  { manufacturer: manufacturers.first, name: 'MacBook Pro (15-inch, 2017)' },
+  { manufacturer: manufacturers.second, name: 'V206HQLABMD 19.5"' },
+  { manufacturer: manufacturers.third, name: 'SE450 Series LS24E45KDLV/GO 23.6"' }
+])
+
+locations = Location.create([
+  {
+      name: 'In Primary User\'s Possession'
+  },
+  {
+      name: 'Office',
+      address_line1: '1234 Main Street',
+      address_line2: 'Third Floor',
+      city: 'CITY',
+      state: 'XX',
+      zip_code: '12345'
+  }
+])
+
+assets = Asset.create([
+  {
+      asset_tag: 'LA20001',
+      serial_number: 'FAKESERIAL123456789',
+      category: categories.first,
+      model: models.second,
+      primary_user: 'Jane Doe',
+      location: locations.second,
+      purchase_date: '2020-01-01',
+      warranty_expiry: '2021-01-01',
+      notes: 'This is a note'
+  }
+])
